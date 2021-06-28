1b536d38c20da1c6e28e75cafdb0a4e5d57ecdc5
Joshua Allen <joshuaallen@Joshuas-MacBook-Pro.local>
2021-06-28 11:53:35 -0700

    added mechaCar mpg analysis and updated readme

---
+++ MechaCar_mpg.R
@@ -0,0 +1,19 @@
+# Deliverable 1
+
+# Step 3. Use Library function to load dplyr package
+library(dplyr)
+
+
+# Step 4. Import and read in the MechaCar_mpg.csv file as a dataframe.
+mechaCar_mpg_table <- read.csv(file='MechaCar_mpg.csv', check.names=F,stringsAsFactors = F)
+
+
+# Step 5. Perform linear regression using the lm() function. 
+# In the lm() function, pass in all six variables (i.e., columns), 
+# and add the dataframe you created in Step 4 as the data parameter.
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar_mpg_table)
+
+
+# Step 6. Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar_mpg_table)
+)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MechaCar_Statistical_Analysis
 Analysis of MechaCar performance using statistical testing
+
+
+
+## Linear Regression to Predict MPG
+**** Insert summary screenshot ****
